2151f20617c0c2e3d0fea2fd7fae4b697fa4f5ff
samekmd <samuelalkmc@gmail.com>
2024-04-19 17:56:00 -0300

    Adicionei o repositório

---
+++ Pratica04/src/modelo/Aluno.java
@@ -0,0 +1,50 @@
+package modelo;
+
+public class Aluno {
+
+    private String cpf;
+    private String nome;
+    private String dataNasc;
+    private int altura;
+    private int peso;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+}
